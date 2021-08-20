9c32c143b93f73ade99a6da8f7f45770eb20357b
rlaehdwozld <79123524+rlaehdwozld@users.noreply.github.com>
2021-08-20 14:18:52 +0900

    testtoken

---
+++ src/main/java/com/cos/testtoken/TesttokenApplication.java
@@ -0,0 +1,13 @@
+package com.cos.testtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/testtoken/TesttokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.testtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
